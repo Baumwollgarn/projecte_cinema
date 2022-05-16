076c171d0a917b3b0c4a40e75d6d4db05f368edb
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2022-05-16 13:59:07 +0200

    managed to show for each cinema the pop-up, needs to get extracted from cinema card to get rid of "show"-bug

--- webserver/js/main.js
+++ webserver/js/main.js
@@ -21,7 +21,6 @@ function getMovies() {
                 let movie = movies[i];
                 let card = document.createElement("div");
                 card.className = "card"
-                card.onclick = showShowTimes(data.ID)
                 card.id = movie.id
                 card.innerHTML = `
             <img src="${data.image}" alt="cine_1" class="card-image">
@@ -44,16 +43,54 @@ fetch('https://picsum.photos/v2/list?page=2&limit=13')
 let cardContent = document.createElement('div');
 cardContent.className = 'cinemas';
 
-function createCard(data,id) {
+function createCard(data) {
     let card = document.createElement('div');
     card.className = 'cinema-card';
     card.id = data.id
-    card.addEventListener('click', createShowTimes(data.id))
+    card.addEventListener('click', function (e) {
+        createShowTimes(data.id);
+    });
+    card.addEventListener('click', function (e) {
+        showShowTimesCinema(data.id)
+    })
     card.innerHTML = `
     <img src="${data.download_url}" alt="cine_1" class="cinema-image">
                 <h3 class="cinema-title">Cine ${data.author}</h3>
                 <p class="cinema-address">Carrer de la Rambla, 1, 08002 Barcelona</p>
                 <p class="cinema-phone">${data.width} ${data.height}</p>
+                    <div class="showtimes st-unique" id="schedule-${data.id}">
+        <h3 class="showtimes-title">Bienvenidos al ${data.author}</h3>
+        <div class="cinema-selector-max">
+            <form action="" class="cinema-selector_form">
+                <label for="movie-selector"></label>
+                <select name="movie-selector" id="movie-selector" class="movie-selector">
+                    <option value="0" selected>Selecciona una pelicula</option>
+                </select>
+            </form>
+        </div>
+
+        <div class="showtimes-content">
+            <div class="showtimes-item">
+                <div class="showtimes-item-content">
+                    <h4 class="showtimes-item-title">Sessio 1</h4>
+                    <p class="showtimes-item-time">10:00 - 12:00</p>
+                </div>
+            </div>
+            <div class="showtimes-item">
+                <div class="showtimes-item-content">
+                    <h4 class="showtimes-item-title">Sessio 2</h4>
+                    <p class="showtimes-item-time">12:00 - 14:00</p>
+                </div>
+            </div>
+            <div class="showtimes-item">
+                <div class="showtimes-item-content">
+                    <h4 class="showtimes-item-title">Sessio 3</h4>
+                    <p class="showtimes-item-time">14:00 - 16:00</p>
+                </div>
+            </div>
+        </div>
+        <i class="fa-solid fa-times close" onclick="hideShowTimes()"> Tancar</i>
+    </div>
                `
     cardContent.appendChild(card);
     document.querySelector('.cinemas').appendChild(cardContent);
@@ -108,16 +145,48 @@ function changeLanguage (language) {
 
 const cinema = document.querySelector('.cinema-card');
 
-cinema.addEventListener('click', function (e) {
+/*cinema.addEventListener('click', function (e) {
     if (e.target.classList.contains('cinema-card')) {
         createShowTimes(e.target.id);
     }
-});
+});*/
 
-function createShowTimes (id) {
+
+/*function createShowTimes (id) {
     let showTimes = document.createElement('div');
-    showTimes.className = 'show-times';
+    showTimes.className = 'showtimes';
     showTimes.innerHTML = `
-    <h3 class="show-times-title">Show Times</h3>
+    <div class="schedule" id="schedule-${id}">
+            <h3 class="schedule-title">Horari</h3>
+            <div class="schedule-content">
+                <div class="schedule-item">
+                    <div class="schedule-item-content">
+                        <h4 class="schedule-item-title">Sessio 1</h4>
+                        <p class="schedule-item-time">10:00 - 12:00</p>
+                    </div>
+                </div>
+                <div class="schedule-item">
+                    <div class="schedule-item-content">
+                        <h4 class="schedule-item-title">Sessio 2</h4>
+                        <p class="schedule-item-time">12:00 - 14:00</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     `
+    document.querySelector('.cinemas').appendChild(showTimes);
+}*/
+
+function showShowTimesCinema(id) {
+    
+    let schedule = document.querySelector(`#schedule-${id}`);
+    let dark = document.querySelector('.dark');
+
+    if (schedule.style.display === "none") {
+        schedule.style.display = "flex";
+        dark.style.display = "flex";
+    } else {
+        schedule.style.display = "none";
+        dark.style.display = "none";
+    }   
 }
