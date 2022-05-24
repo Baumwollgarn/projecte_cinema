924f8071358412bc29348579505fdeaf0426a0b2
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2022-05-24 09:57:24 +0200

    Updated schedule window pop up with 2 selects for each movie & sessions

--- webserver/js/main.js
+++ webserver/js/main.js
@@ -1,36 +1,19 @@
-// Function to hide "showTimes"
-function hideShowTimes(id) {
-    document.getElementById("showTimes").style.display = "none";
-    document.getElementById("dark").style.display = "none";
-    document.getElementById("schedule-" + {id}).style.display = "none";
+/*
+function createMovOpt (moviedata2) {
+    for (let i = 0; i < moviedata2.data.length; i++) {
+        let option = document.createElement("option");
+        option.innerHTML = moviedata2.data[i].generic;
+        option.value = moviedata2.data[i].idMovie;
+    }
 }
 
-function showShowTimes(id) {
-    document.getElementById("showTimes").style.display = "flex";
-    document.getElementById("dark").style.display = "flex";
-}
-
-
-
-
-fetch('http://localhost:8081/daw/select * from movie')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(movieData => {
-        for (let i = 0; i < movieData.data.length; i++) {
-            createMovieCard(movieData.data[i]);
-        }
-    })
-
-fetch('http://localhost:8081/daw/select * from cinema')
+fetch('http://localhost:8081/daw/select idMovie, generic from movie;')
     .then(response => response.json())
-    .then(data => {
-        for (let i = 0; i < data.data.length; i++) {
-            createCard(data.data[i]);
-            
-        }
+    .then(moviedata2 => {
+        createMovOpt(moviedata2);
     })
+    .catch(error => console.log(error));
+*/
 
 function createMovieCard(movieData) {
     let movieCard = document.createElement('div')
@@ -40,7 +23,7 @@ function createMovieCard(movieData) {
     <img src="${movieData.image}" alt="${movieData.generic}" class="card-image" onclick="openTrailerPopUp(${movieData.idMovie})">
             <h3 class="card-title">${movieData.generic}</h3>`
     document.querySelector('.movieCards').appendChild(movieCard)
-    
+
 }
 
 let cardContent = document.createElement('div');
@@ -59,76 +42,33 @@ function createCard(data) {
                 <p class="cinema-address">${data.address}</p>
                 <p class="cinema-phone">${data.phoneNumber}</p>
         <div class="showtimes st-unique" id="schedule-${data.idCinema}">
-            <h3 class="showtimes-title">Bienvenidos al ${data.name}</h3>
+            <h3 class="showtimes-title">Bienvenidos al <b>${data.name}</b></h3>
+            <h5>Selecciona una pel·licula</h5>
             <div class="cinema-selector-max">
                 <form action="" class="cinema-selector_form">
                     <label for="movie-selector"></label>
-                    <select name="movie-selector" id="movie-selector" class="movie-selector">
-                        <option value="0" selected>Selecciona una pelicula</option>
+                    <select name="movie-selector" id="movie-selector-${data.idCinema}" class="movie-selector">
                     </select>
+                    <label for="session-selector"></label>
+                    <select name="session-selector" id="session-selector-${data.idCinema}" class="session-selector">
+                    <option value="none">Selecciona una sessió</option>
+                    <option value="09">09:00 - 11:00</option>
+                    <option value="11">11:00 - 13:00</option>
+                    <option value="13">13:00 - 15:00</option>
+                    <option value="15">15:00 - 17:00</option>
+                    <option value="17">17:00 - 19:00</option>
+                    <option value="19">19:00 - 21:00</option>
+                    <option value="21">21:00 - 23:00</option>
+                    </select>
+                    <button type="submit" class="submit-button">
+                        <i class="fas fa-ticket-alt"></i>
+                        <span>Comprar</span>
                 </form>
             </div>
-
-        <div class="showtimes-content">
-            <div class="showtimes-item">
-                <div class="showtimes-item-content">
-                    <h4 class="showtimes-item-title">Sessio 1</h4>
-                    <p class="showtimes-item-time">10:00 - 12:00</p>
-                </div>
-            </div>
-            <div class="showtimes-item">
-                <div class="showtimes-item-content">
-                    <h4 class="showtimes-item-title">Sessio 2</h4>
-                    <p class="showtimes-item-time">12:00 - 14:00</p>
-                </div>
-            </div>
-            <div class="showtimes-item">
-                <div class="showtimes-item-content">
-                    <h4 class="showtimes-item-title">Sessio 3</h4>
-                    <p class="showtimes-item-time">14:00 - 16:00</p>
-                </div>
-            </div>
-        </div>
     </div>
                `
     cardContent.appendChild(card);
     document.querySelector('.cinemas').appendChild(cardContent);
-}
-
-
-function openMobileMenu() {
-    if (document.getElementById("menu-mobile-content").classList.contains("closed")) {
-        document.getElementById("menu-mobile-content").style.display = "block";
-        document.getElementById("menu-mobile-content").classList.add("open");
-        document.getElementById("menu-mobile-content").classList.remove("closed");
-        document.getElementById("menu-mobile-content").classList.add("animated");
-
-    } else if (document.getElementById("menu-mobile-content").classList.contains("open")) {
-        document.getElementById("menu-mobile-content").classList.remove("open");
-        document.getElementById("menu-mobile-content").classList.add("closed");
-        document.getElementById("menu-mobile-content").style.display = "none";
-
-    }
-}
-
-function closeMobileMenu() {
-    if (document.getElementById("menu-mobile-content").style.display === "block" && window.innerWidth > 900) {
-        document.getElementById("menu-mobile-content").style.display = "none";
-        document.getElementById("menu-mobile-content").classList.remove("open");
-        document.getElementById("menu-mobile-content").classList.add("closed");
-    }
-}
-
-window.addEventListener("resize", closeMobileMenu);
-
-
-function showShowTimesCinema(id) {
-
-    let schedule = document.querySelector(`#schedule-${id}`);
-    let dark = document.querySelector('.dark');
-
-    schedule.style.display = "flex";
-    dark.style.display = "flex";
 }
 
 fetch('http://localhost:8081/daw/select * from cinema')
@@ -150,43 +90,39 @@ fetch('http://localhost:8081/daw/select * from cinema')
     })
     .catch(error => console.log(error));
 
-function hideShowTimesCinema(id) {
 
-    let schedule = document.querySelector(`.st-unique`);
-    let dark = document.querySelector('.dark');
-
-    schedule.style.display = "none";
-    dark.style.display = "none";
-}
-
-function closeAll() {
-    let schedule = document.querySelectorAll(`.st-unique`)
-    let dark = document.querySelector('.dark')
-    let trailerCard = document.querySelectorAll('.trailerCard');
-    
-    schedule.forEach(schedule => {
-        schedule.style.display = "none";
-    });
-    dark.style.display = "none";
-    trailerCard.forEach(card => {
-        card.style.display = "none";
-    });
-}
-
-
-fetch('http://localhost:8081/daw/SELECT idMovie,generic FROM movie;')
-    .then(response => response.json())
-    .then(moviedata2 => {
-        for (let i = 0; i < moviedata2.data.length; i++) {
-            addMovieOption(moviedata2.data[i]);
+fetch('http://localhost:8081/daw/select * from movie')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(movieData => {
+        for (let i = 0; i < movieData.data.length; i++) {
+            createMovieCard(movieData.data[i]);
         }
     })
-    .catch(error => console.log(error))
 
-let addMovieOption = (movie) => {
-    let movieSelect = document.querySelector('#movie-selector');
-    let option = document.createElement('option');
-    option.value = movie.idMovie;
-    option.innerHTML = movie.generic;
-    movieSelect.appendChild(option);
-}
+fetch('http://localhost:8081/daw/select * from cinema')
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < data.data.length; i++) {
+            createCard(data.data[i]);
+        }}
+    )
+    .then (() => {
+        document.querySelectorAll('.cinema-card').forEach(cinema => {
+            let idCinema = cinema.id;
+            fetch(`http://localhost:8081/daw/select * from movie`)
+                .then(response => response.json())
+                .then(data => {
+                    let movieSelector = document.querySelector(`#movie-selector-${idCinema}`);
+                    movieSelector.innerHTML = `<option value="0">Selecciona una pelicula</option>`;
+                    data.data.forEach(movie => {
+                        movieSelector.innerHTML += `<option value="${movie.idMovie}">${movie.generic}</option>`;
+                    });
+                })
+                .catch(error => console.log(error));
+        });
+    })
+    .catch(error => console.log(error));
+
+
